ad2fc0a81da298c83939c52287b13738debdfe8f
remkv <154243179+remkv@users.noreply.github.com>
2024-01-06 23:39:04 -0500

    Day 3 tasks

---
+++ first.js
@@ -0,0 +1,9 @@
+let obj1 = {name:"Person 1", age:5};
+let obj2 = {age :5, name:"Person 1"};
+
+
+if(obj1.name === obj2.name && obj1.age === obj2.age){
+    console.log("JSON are equal")
+}  else{
+    console.log("Not equal");
+}
