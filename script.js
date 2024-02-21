70e0dbd37a950d73ca448a013bcfc6c980c1e046
remkv <154243179+remkv@users.noreply.github.com>
2024-02-21 15:16:30 -0500

    Call Back Hell Task-Day 16

---
+++ script.js
@@ -0,0 +1,41 @@
+function startTimer(){
+    const h1 = document.createElement('h1');
+    h1.style.cssText += 'color: blue; text-align:center';
+    document.getElementById("root").appendChild(h1);
+
+setTimeout(function() {   
+    h1.innerHTML = "10";  
+    setTimeout(function() { 
+        h1.innerHTML = "9"; 
+        setTimeout(function() {
+            h1.innerHTML = "8"; 
+            setTimeout(function() {
+                h1.innerHTML = "7"; 
+                setTimeout(function() {
+                    h1.innerHTML = "6"; 
+                    setTimeout(function() {
+                        h1.innerHTML = "5"; 
+                        setTimeout(function() {
+                            h1.innerHTML = "4"; 
+                            setTimeout(function() {
+                                h1.innerHTML = "3"; 
+                                setTimeout(function() {
+                                    h1.innerHTML = "2"; 
+                                    setTimeout(function() {
+                                        h1.innerHTML = "1"; 
+                                        setTimeout(function() {
+                                            h1.innerHTML = "Happy Independence Day";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+}
+
+
